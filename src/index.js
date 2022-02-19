7913e0fd28f34a78036805be297871a4e76c831e
3847599+ianhuynh@users.noreply.github.com <3847599+ianhuynh@users.noreply.github.com>
2022-02-18 17:49:21 -0800

    upgrade libraries, change HelloWorld to App, changed port to 3000, added eslint

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React from "react";
+import { render } from "react-dom";
 
-import HelloWorld from './HelloWorld';
+import App from "./App";
 
-render(<HelloWorld />, document.getElementById('root'));
+render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
